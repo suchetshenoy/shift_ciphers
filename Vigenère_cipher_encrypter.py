a7d2357bbaecd027c1eaac80af8f5b968ccbf674
Suchet Shenoy <67583672+suchetshenoy@users.noreply.github.com>
2023-08-12 22:52:54 +0530

    Update Vigenère_cipher_encrypter.py

--- Vigenère_cipher_encrypter.py
+++ Vigenère_cipher_encrypter.py
@@ -3,11 +3,11 @@ plain_text = "hello"
 key = "hello"#key must be as long as text at least
 plain_text = plain_text.lower()
 key = key.upper()
-encrypted_word = ""
+encrypted_text = ""
 for i in range(len(plain_text)):
   if plain_text[i] in "abcdefghijklmnopqrstuvwxyz":
-    encrypted_word += chr(((ord(plain_text[i])-ord("a")+ord(key[i])-ord("A"))%26)+ord("a"))
+    encrypted_text += chr(((ord(plain_text[i])-ord("a")+ord(key[i])-ord("A"))%26)+ord("a"))
   else:
     encrypted_word+=plain_text[i]
 
-print(encrypted_word)
+print(encrypted_text)
