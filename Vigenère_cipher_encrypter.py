67f60e5446970fa848a71fc6635fd7262005b557
Suchet Shenoy <67583672+suchetshenoy@users.noreply.github.com>
2023-08-12 22:51:10 +0530

    Create Vigenère_cipher_encrypter.py

---
+++ Vigenère_cipher_encrypter.py
@@ -0,0 +1,13 @@
+#Vigenère cipher encrypter
+plain_text = "hello"
+key = "hello"#key must be as long as text at least
+plain_text = plain_text.lower()
+key = key.upper()
+encrypted_word = ""
+for i in range(len(plain_text)):
+  if plain_text[i] in "abcdefghijklmnopqrstuvwxyz":
+    encrypted_word += chr(((ord(plain_text[i])-ord("a")+ord(key[i])-ord("A"))%26)+ord("a"))
+  else:
+    encrypted_word+=plain_text[i]
+
+print(encrypted_word)
